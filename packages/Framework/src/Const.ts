1384ae96fc9930049094b0c5e0f598f1649b1fbe
yct37785 <yct37785@gmail.com>
2025-12-19 18:10:04 +0800

    remove design tokens from const, update AppBar stylesheet to use new theme dessign token

--- packages/Framework/src/Const.ts
+++ packages/Framework/src/Const.ts
@@ -1,37 +1,7 @@
 /******************************************************************************************************************
- * Styles
- ******************************************************************************************************************/
-export const padSize025 = 3;
-export const padSize05 = 4;
-export const padSize = 8;
-
-export const radiusSmall = 4;
-export const radiusMedium = 8;
-export const radiusLarge = 12;
-
-export const rippleColorForLight = 'rgba(0,0,0,0.12)';
-export const rippleColorForDark = 'rgba(255,255,255,0.12)';
-
-export const snackbarDuration = 2000;
-
-export const textOpacityHEmphasis = 0.87;
-export const textOpacityMEmphasis = 0.6;
-
-export const iconSizeSmall = 20;
-export const iconSizeMedium = 30;
-
-export const pressOpacityLight = 0.8;
-export const pressOpacityMedium = 0.5;
-export const pressOpacityHeavy = 0.2;
-export const pressInDurationMS = 110;
-export const pressOutDurationMS = 160;
-
-export const animDuration = 300;
-
-/******************************************************************************************************************
  * Keys.
  ******************************************************************************************************************/
-// reserved default values for storage
+// reserved default values for storage (move to defaults.ts)
 export const localDataDefaults = {
   isDarkMode: false,
   language: 'en',
@@ -40,7 +10,7 @@ export const localDataDefaults = {
 /******************************************************************************************************************
  * Misc.
  ******************************************************************************************************************/
-// logging
+// logging (move to theme)
 export const logColors = {
   reset: '\x1b[0m',
   bright: '\x1b[1m',
