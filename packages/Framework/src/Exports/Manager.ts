9f397f74ddd2faa6fbf75a6c0a6131cfed73eb24
YC-ACER-LAPTOP\yctan <yct37785@gmail.com>
2025-12-27 00:52:11 +0800

    Remove Firestore for now

--- packages/Framework/src/Exports/Manager.ts
+++ packages/Framework/src/Exports/Manager.ts
@@ -1,5 +1,4 @@
 export * from '../Manager/Firebase/Auth/FirebaseAuthManager';
-export * from '../Manager/Firebase/FirebaseFirestoreManager';
 export * from '../Manager/AppSettingsManager';
 export * from '../Manager/AppThemeManager';
 export * from '../Manager/LocalDataManager';
