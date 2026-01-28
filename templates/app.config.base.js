ab1025c5ac3a3a00186597970c32b698ef84c4ef
YC-ACER-LAPTOP\yctan <yct37785@gmail.com>
2026-01-28 21:46:02 +0800

    google-services.json not google-LocalData.json

--- templates/app.config.base.js
+++ templates/app.config.base.js
@@ -17,8 +17,8 @@ export default function withBaseConfig({ config }) {
       // ---- platform config that is common to all apps ----
       android: {
         ...(config?.expo?.android ?? {}),
-        // each app should place its google-LocalData.json in its own folder
-        googleServicesFile: './google-LocalData.json',
+        // each app should place its google-services.json in its own folder
+        googleServicesFile: './google-services.json',
 
         // IMPORTANT: package is app-specific; do NOT set here
         // package: 'com.mycorp.myapp'
