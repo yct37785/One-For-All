d5715ba82517c03c1e01edc20d82cffccfa7bfbe
YC-ACER-LAPTOP\yctan <yct37785@gmail.com>
2026-02-01 13:46:24 +0800

    Logic module handles barrel export

--- packages/platform/index.ts
+++ packages/platform/index.ts
@@ -1,9 +1,1 @@
-export * as App from './src/Logic/App';
-export * as Comp from './src/Logic/Component';
-export * as Defaults from './src/Logic/Defaults';
-export * as FB from './src/Logic/Firebase';
-export * as LocalData from './src/Logic/LocalData';
-export * as Nav from './src/Logic/Nav';
-export * from './src/Logic/Root';
-export * as UI from './src/Logic/UI';
-export * as Util from './src/Logic/Util';
+export * from './src/Logic';
